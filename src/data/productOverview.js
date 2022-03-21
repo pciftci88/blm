4c714db6c7e3a8a95b54889c96ee1b8848dd6aab
pciftci88 <philipp.ciftci@gmail.com>
2022-03-21 14:57:30 +0100

    New products on startpage

--- src/data/productOverview.js
+++ src/data/productOverview.js
@@ -4,6 +4,18 @@ import product3 from '../images/products/startpage/product_3.jpg';
 import product4 from '../images/products/startpage/product_4.jpg';
 import product5 from '../images/products/startpage/product_5.jpg';
 import product6 from '../images/products/startpage/product_6.jpg';
+import product7 from '../images/products/startpage/product_7.jpg';
+import product8 from '../images/products/startpage/product_8.jpg';
+import product9 from '../images/products/startpage/product_9.jpg';
+import product10 from '../images/products/startpage/product_10.jpg';
+import product11 from '../images/products/startpage/product_11.jpg';
+import product12 from '../images/products/startpage/product_12.jpg';
+import product13 from '../images/products/startpage/product_13.jpg';
+import product14 from '../images/products/startpage/product_14.jpg';
+import product15 from '../images/products/startpage/product_15.jpg';
+import product16 from '../images/products/startpage/product_16.jpg';
+import product17 from '../images/products/startpage/product_17.jpg';
+import product18 from '../images/products/startpage/product_18.jpg';
 
 const productOverviewImages = [
   {
@@ -29,7 +41,55 @@ const productOverviewImages = [
   {
     "src": product6,
     "description": "Gestra RK 71 PN 6/10/16 check valve"
-  }
+  },
+  {
+    "src": product7,
+    "description": "Leuze HT23/4X-M8 Diffuse sensor with background suppression"
+  },
+  {
+    "src": product8,
+    "description": "Euchner TP1-538A024MC1855 Safety Switch"
+  },
+  {
+    "src": product9,
+    "description": "Wika PCA2MA Pressure Switch"
+  },
+  {
+    "src": product10,
+    "description": "ACE HB-70-400 Hydraulic Damper"
+  },
+  {
+    "src": product11,
+    "description": "Beta C3-T548H-C03-S0-SL-I temperature switch"
+  },
+  {
+    "src": product12,
+    "description": "Hoyer HMA2 132M1 4p B35 Motor"
+  },
+  {
+    "src": product13,
+    "description": "Baumüller GDM012Z-44U20020858/0400×1-C2RSR Disc motor"
+  },
+  {
+    "src": product14,
+    "description": "Schneider Electric LC1D65AM7 TeSys Deca contactor"
+  },
+  {
+    "src": product15,
+    "description": "Beckhoff KL5151 incremental encoder"
+  },
+  {
+    "src": product16,
+    "description": "A-Ryung AGP-15C-CT Pump"
+  },
+  {
+    "src": product17,
+    "description": "Allweiler AEB2N100-IE/011G0K YL 113PP Eccentric pump with geared motor"
+  },
+  {
+    "src": product18,
+    "description": "TR Electronic CEV582M-10167 Rotary Encorder"
+  },
 ]
 
 export default productOverviewImages;
